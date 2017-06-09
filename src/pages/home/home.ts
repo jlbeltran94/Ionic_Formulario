30f1caf863ecbd449081daecd6c484b1fb304693
José Luis Beltrán <jlbeltran94@gmail.com>
2017-06-09 01:46:04 -0500

    formulario listo, faltan arreglos esteticos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,35 +18,7 @@ export class HomePage {
     this.service.getForm().subscribe(
       response => {
         this.form = response._body;
-        this.body = JSON.parse(this.form);
-        console.log(this.form);
-        console.log(this.body.length);
-        console.log(this.body[0]);
-        for (let i = 0; i < this.body.length; i++) {
-          switch (this.body[i].type) {
-            case 'textarea': { 
-              console.log(i + "es textarea"); }
-              break;
-            case 'text': { console.log(i + "es text"); }
-              break;
-            case 'time': { console.log(i + "es time"); }
-              break;
-            case 'radio': { console.log(i + "es radio"); }
-              break;
-            case 'checkbox': { console.log(i + "es checkbox"); }
-              break;
-            case 'select': { 
-              this.values = this.body[i].values;
-              console.log(this.values);
-              console.log(i + "es select"); }
-              break;
-            case 'date': {
-              console.log(i + "es date");
-            }
-              break;
-
-          }
-        }
+        this.body = JSON.parse(this.form);             
 
       }
     );
